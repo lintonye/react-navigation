7768e996c0913b00dfc5f4292a04ed44cfcb586a
Linton Ye <linton@jimulabs.com>
2017-02-20 13:44:57 -0800

    TransitionView: clone as animated component

--- src/views/Transition/TransitionView.js
+++ src/views/Transition/TransitionView.js
@@ -5,9 +5,28 @@ import {
   View,
   UIManager,
   findNodeHandle,
+  Animated,
 } from 'react-native';
 
 import { TransitionItem } from './TransitionItems';
+
+function cloneAsAnimatedComponent(element: React.Element<*>) {
+  const isStatefulComponent = type => type.prototype && typeof type.prototype.render === 'function';
+  if (typeof element.type === 'function' && element.type.name !== 'AnimatedComponent') {
+    if (isStatefulComponent(element.type)) {
+      const type = Animated.createAnimatedComponent(element.type);
+      return React.createElement(type, element.props, element.props.children);
+    } else {
+      return (
+        <Animated.View {...element.props}>
+          {element}
+        </Animated.View>
+      );
+    }
+  } else {
+    return element;
+  }
+}
 
 class TransitionView extends Component {
   _view: any;
@@ -18,24 +37,30 @@ class TransitionView extends Component {
   render() {
     // collapsable={false} is required for UIManager.measureInWindow to get the actual measurements
     // instead of undefined, see https://github.com/facebook/react-native/issues/9382
-    return (
+    /*return (
       <View collapsable={false}
-        ref={c => this._view = c}>
-        {this.props.children}
+        ref={c => this._view = c} style={{ flex: 1 }}>
+        {this._getAnimatedChild()}
       </View>
-    )
+    )*/
+    return this._getAnimatedChild();
   }
+
+  _getAnimatedChild() {
+    return cloneAsAnimatedComponent(React.Children.only(this.props.children));
+  }
+
   componentDidMount() {
     const { registerTransitionView } = this.context;
     if (!registerTransitionView) return;
 
-    const { name, containerRouteName } = this.props;
+    const { id, containerRouteName } = this.props;
     const nativeHandle = findNodeHandle(this._view);
     registerTransitionView(new TransitionItem
       (
-      name,
+      id,
       containerRouteName,
-      React.Children.only(this.props.children),
+      this._getAnimatedChild(),
       nativeHandle,
     ));
   }
