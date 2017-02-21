255e2a7a149e00475a912e07e108c6d528630891
Linton Ye <linton@jimulabs.com>
2017-02-21 10:02:12 -0800

    Add Transition.Text

--- src/views/Transition/index.js
+++ src/views/Transition/index.js
@@ -1,6 +1,7 @@
 import {
   View,
   Image,
+  Text,
 } from 'react-native';
 import TransitionItems from './TransitionItems';
 import createTransitionComponent from './createTransitionComponent';
@@ -8,6 +9,6 @@ import createTransitionComponent from './createTransitionComponent';
 export default {
   View: createTransitionComponent(View),
   Image: createTransitionComponent(Image),
-  Items: TransitionItems,
+  Text: createTransitionComponent(Text),
   createTransitionComponent,
 }
