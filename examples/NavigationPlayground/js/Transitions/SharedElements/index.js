ee20d5e473f2d406848669a7b9375444cc236763
Linton Ye <linton@jimulabs.com>
2017-02-22 09:37:55 -0800

    Implement shared element transition

--- examples/NavigationPlayground/js/Transitions/SharedElements/index.js
+++ examples/NavigationPlayground/js/Transitions/SharedElements/index.js
@@ -5,6 +5,7 @@ import {
 import PhotoGrid from './PhotoGrid';
 import PhotoDetail from './PhotoDetail';
 import {Transition} from 'react-navigation';
+import _ from 'lodash';
 
 const {createTransition} = Transition;
 
@@ -15,7 +16,36 @@ const SharedElements = (filter) => ({
     itemsOnFromRoute: Array<*>, 
     itemsOnToRoute: Array<*>, 
     transitionProps) {
-      //TODO
+      const itemIdsOnBoth = _.intersectionWith(itemsOnFromRoute, itemsOnToRoute, (i1, i2) => i1.id === i2.id)
+        .map(item => item.id);
+      const {progress} = transitionProps;
+      const createSharedItemStyle = (result, id) => {
+        const fromItem = itemsOnFromRoute.find(item => item.id === id);
+        const toItem = itemsOnFromRoute.find(item => item.id === id);
+        const inputRange = [0, 1];
+        const left = progress.interpolate({
+          inputRange, outputRange: [fromItem.metrics.x, toItem.metrics.x]
+        });
+        const top = progress.interpolate({
+          inputRange, outputRange: [fromItem.metrics.y, toItem.metrics.y]
+        });
+        const width = progress.interpolate({
+          inputRange, outputRange: [fromItem.metrics.width, toItem.metrics.width]
+        });
+        const height = progress.interpolate({
+          inputRange, outputRange: [fromItem.metrics.height, toItem.metrics.height]
+        });
+        result[id] = {left, top, width, height, right: null, bottom: null};
+        return result;
+      };
+      const createHideStyle = (result, id) => {
+        result[id] = { opacity: 0 };
+        return result;
+      };
+      return {
+        from: itemIdsOnBoth.reduce(createSharedItemStyle, {}),
+        to: itemIdsOnBoth.reduce(createHideStyle, {}),
+      }
   }
 });
 
@@ -52,10 +82,10 @@ const SharedImage = createTransition(SharedElements, /image-.+/);
 const CrossFadeScene = createTransition(CrossFade, /\$scene.+/);
 
 const transitions = [
-  { from: 'PhotoGrid', to: 'PhotoDetail', transition: CrossFadeScene },
-  { from: 'PhotoDetail', to: 'PhotoGrid', transition: CrossFadeScene },
-  // { from: 'PhotoGrid', to: 'PhotoDetail', transition: SharedImage },
+  // { from: 'PhotoGrid', to: 'PhotoDetail', transition: CrossFadeScene },
   // { from: 'PhotoDetail', to: 'PhotoGrid', transition: CrossFadeScene },
+  { from: 'PhotoGrid', to: 'PhotoDetail', transition: SharedImage },
+  { from: 'PhotoDetail', to: 'PhotoGrid', transition: SharedImage },
 ];
 
 const App = StackNavigator({
