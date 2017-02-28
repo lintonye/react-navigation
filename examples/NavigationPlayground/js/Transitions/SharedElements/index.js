038bd0559ac1ea3e798f634784e0c73d64a21beb
Linton Ye <linton@jimulabs.com>
2017-02-28 11:25:01 -0800

    Add staggeredAppear example

--- examples/NavigationPlayground/js/Transitions/SharedElements/index.js
+++ examples/NavigationPlayground/js/Transitions/SharedElements/index.js
@@ -6,6 +6,7 @@ import PhotoGrid from './PhotoGrid';
 import PhotoDetail from './PhotoDetail';
 import { Transition } from 'react-navigation';
 import _ from 'lodash';
+import faker from 'faker';
 
 const {createTransition, together} = Transition;
 
@@ -106,7 +107,7 @@ const DelayedFadeInToRoute = (filter) => ({
         return result;
       }, {}),
     }
-  }  
+  }
 });
 
 const FastFadeOutFromRoute = (filter) => ({
@@ -125,7 +126,7 @@ const FastFadeOutFromRoute = (filter) => ({
         return result;
       }, {}),
     }
-  }  
+  }
 });
 
 const SharedImage = createTransition(SharedElements, /image-.+/);
@@ -134,11 +135,77 @@ const CrossFadeScene = createTransition(CrossFade, /\$scene.+/);
 const DelayedFadeInDetail = createTransition(DelayedFadeInToRoute, /\$scene-PhotoDetail/);
 const FastFadeOutDetail = createTransition(FastFadeOutFromRoute, /\$scene-PhotoDetail/);
 
+// TODO slide doesn't seem easy to implement, perhaps need to expose current route index and interpolate position instead of progress?
+const Slide = (filter) => ({
+  filter,
+  createAnimatedStyleMap(
+    itemsOnFromRoute: Array<*>, 
+    itemsOnToRoute: Array<*>, 
+    transitionProps) {
+    const {layout: {initWidth}, progress} = transitionProps;
+    const createStyle = (items, onFromRoute: boolean) => items.reduce((result, item) => {
+      const translateX = progress.interpolate({
+        inputRange: [0, 0.05, 1],
+        outputRange: onFromRoute ? [0, -initWidth, -initWidth] : [initWidth, 0, 0],
+      })
+      result[item.id] = { transform: [{ translateX }] }
+      return result;
+    }, {});
+    return {
+      from: createStyle(itemsOnFromRoute, true),
+      to: createStyle(itemsOnToRoute, false),
+    };
+  }
+});
+
+const StaggeredAppear = (filter) => ({
+  filter,
+  createAnimatedStyleMap(
+    itemsOnFromRoute: Array<*>, 
+    itemsOnToRoute: Array<*>, 
+    transitionProps) {
+    const createStyle = (startTime, axis, direction) => {
+      const {progress} = transitionProps;
+      const inputRange = [0, startTime, 1];
+      const opacity = progress.interpolate({
+        inputRange,
+        outputRange:  [0, 0, 1],
+      });
+      const translate = progress.interpolate({
+        inputRange,
+        outputRange: [ direction * 400, direction * 400, 0],
+      });
+      axis = axis === 'x' ? 'translateX' : 'translateY';
+      return {
+        opacity,
+        transform: [ { [axis]: translate } ],
+      };
+    }
+    const clamp = (x, min, max) => Math.max(min, Math.min(max, x));
+    const axes = ['x', 'y'];
+    const directions = [-1, 1];
+    return {
+      to: itemsOnToRoute.reduce((result, item) => {
+        const startTime = clamp(Math.random(), 0.1, 0.9);
+        const axis = faker.random.arrayElement(axes);
+        const direction = faker.random.arrayElement(directions);
+        result[item.id] = createStyle(startTime, axis, direction);
+        return result;
+      }, {}),
+    };
+  }
+})
+
+const SlideScenes = createTransition(Slide, /\$scene-.*/);
+const StaggeredAppearImages = createTransition(StaggeredAppear, /image-.+/);
+
 const transitions = [
   // { from: 'PhotoGrid', to: 'PhotoDetail', transition: CrossFadeScene },
   // { from: 'PhotoDetail', to: 'PhotoGrid', transition: CrossFadeScene },
-  { from: 'PhotoGrid', to: 'PhotoDetail', transition: together(SharedImage, DelayedFadeInDetail)},
-  { from: 'PhotoDetail', to: 'PhotoGrid', transition: together(SharedImage, FastFadeOutDetail) },
+  // { from: 'PhotoGrid', to: 'PhotoDetail', transition: together(SharedImage, DelayedFadeInDetail)},
+  // { from: 'PhotoDetail', to: 'PhotoGrid', transition: together(SharedImage, FastFadeOutDetail) },
+  { from: 'PhotoGrid', to: 'PhotoDetail', transition: CrossFadeScene },
+  { from: 'PhotoDetail', to: 'PhotoGrid', transition: together(StaggeredAppearImages, SlideScenes) },
 ];
 
 const App = StackNavigator({
