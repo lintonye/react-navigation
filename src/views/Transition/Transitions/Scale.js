d2747a7cf6c130e11386989d6dd153e802e12438
Linton Ye <linton@jimulabs.com>
2017-03-07 22:07:23 -0800

    Add Scale transition

---
+++ src/views/Transition/Transitions/Scale.js
@@ -0,0 +1,21 @@
+// @flow
+
+import { createTransition } from '../transitionHelpers';
+
+const Scale = createTransition({
+  createAnimatedStyleMap(
+    itemsOnFromRoute: Array<*>, 
+    itemsOnToRoute: Array<*>, 
+  ) {
+    const scale = (outputRange) => (result, item) => {
+      result[item.id] = { opacity: { outputRange } };
+      return result;
+    }
+    return {
+      from: itemsOnFromRoute.reduce(scale([1, 0]), {}),
+      to: itemsOnToRoute.reduce(scale([0, 1]), {}),
+    }
+  }
+});
+
+export default Scale;
